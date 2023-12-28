2b8eba363c02f3447413c2656be14d14477883ba
badrEladraoui1 <eladraoui.badr@gmail.com>
2023-12-28 23:32:26 +0100

    starting the project

---
+++ faq-accordion-main/faq-accordion-main/Javascript/index.js
@@ -0,0 +1,10 @@
+// // 'ABCDDDEFGHI
+
+// const getLongest = (string) => {
+//   const longest = [];
+//   for (let i = 0; i < string.length - 1; i++) {
+//     if (string[i] === string[i + 1]) {
+//       longest.push();
+//     }
+//   }
+// };
