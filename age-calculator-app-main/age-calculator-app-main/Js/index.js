9c216423067cd43cebfa80baa94c5938dd781828
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-05 23:12:45 +0100

    adding styles

---
+++ age-calculator-app-main/age-calculator-app-main/Js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const year = document.getElementById("YEAR")
+        if (year.innerHTML.length > 0) {
+            if (year.innerHTML.includes([]))
+        }
+    }
+})
