1257c61b36a26a1a81454d3fd2e0f8f8cdf3b701
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-06 18:43:38 +0100

    Js logic

--- age-calculator-app-main/age-calculator-app-main/Js/index.js
+++ age-calculator-app-main/age-calculator-app-main/Js/index.js
@@ -19,19 +19,72 @@ const checkInputs = (day, month, year) => {
     console.log(monthValue);
     console.log(yearValue);
 
-    if (isLeapYear(yearValue)) {
+    checkInputsProcess(dayValue, monthValue, yearValue)
 
+}
+
+const isLeapYear = year => {
+    const yearValue = parseInt(year)
+    if (isNaN(yearValue) || yearValue < 1900 || yearValue > 2100) return "invalid year"
+    return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+
+    // if (year >= new Date().getFullYear || year === null || year === "") {
+    //     return "invalid year"
+    // } else {
+    //     return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+    // }
+
+}
+
+
+const checkInputsProcess = (days, month, year) => {
+
+    const yearValue = parseInt(year)
+    const daysValue = parseInt(days)
+
+    if (isNaN(yearValue) || isNaN(daysValue)) {
+        return "invalid Input"
     }
-}
 
-const isLeapYear = year => ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+    const monthsWith30Days = month == "04" || month == "06" || month == "09" || month == "11"
+    const monthsWith31Days = month == "01" || month == "05" || month == "07" || month == "08" || month == "10" || month == "12"
 
-
-
-const checkDays = () => {
-
-}
-
-const checkMonths = () => {
-
+    if (monthsWith30Days) {
+        if (days.value <= '30') {
+            console.log(days);
+            return "Good value in the days input ."
+        } else {
+            return "Wrong value in the days input ."
+        }
+    } else if (monthsWith31Days) {
+        if (days.value <= '31') {
+            console.log(days);
+            return "Good value in the days input ."
+        } else {
+            return "Wrong value in the days input ."
+        }
+    } else if (month == '2') {
+        if (isLeapYear(year)) {
+            console.log(year);
+            if (days.value <= "29") {
+                console.log(days);
+                return "Good value in the days input ."
+            }
+            else {
+                console.log(days);
+                return "Wrong value in the days input ."
+            }
+        } else {
+            if (days.value <= "28") {
+                console.log(days);
+                return "Good value in the days input ."
+            }
+            else {
+                console.log(days);
+                return "Wrong value in the days input ."
+            }
+        }
+    } else {
+        return "Wrong value in the month input ."
+    }
 }
