0560eb48a078c8f46e9921530abf287a5d963064
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-06 16:20:15 +0100

    starting Js logic

--- age-calculator-app-main/age-calculator-app-main/Js/index.js
+++ age-calculator-app-main/age-calculator-app-main/Js/index.js
@@ -1,8 +1,37 @@
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        const year = document.getElementById("YEAR")
-        if (year.innerHTML.length > 0) {
-            if (year.innerHTML.includes([]))
-        }
+document.getElementById("iconBtn").addEventListener('click', function () { checkInputs("DAY", "MONTH", "YEAR") })
+
+
+const checkInputs = (day, month, year) => {
+
+    const commonDays = /[0-9]|1[0-9]|2[0-8]/
+    const dayRegex30 = `${commonDays}|29|30`
+    const dayRegex31 = `${dayRegex30}|31`
+
+    const dayInput = document.getElementById(day)
+    const monthInput = document.getElementById(month)
+    const yearInput = document.getElementById(year)
+
+    const dayValue = dayInput.value
+    const monthValue = monthInput.value
+    const yearValue = yearInput.value
+
+    console.log(dayValue);
+    console.log(monthValue);
+    console.log(yearValue);
+
+    if (isLeapYear(yearValue)) {
+
     }
-})
+}
+
+const isLeapYear = year => ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+
+
+
+const checkDays = () => {
+
+}
+
+const checkMonths = () => {
+
+}
